9fbfeb99e92f23643aac8e86e46193e598e8980d
TreizyHoyos <treizyyadhirahoyosjulca@gmail.com>
2026-04-27 20:40:55 -0500

    Ingresar parte del try catch

--- src/exament/EXAMENT.java
+++ src/exament/EXAMENT.java
@@ -20,8 +20,48 @@ public class EXAMENT {
         // TODO code application logic here
          Scanner sc = new Scanner(System.in);
         ControladorPaciente controlador = new ControladorPaciente();
-        
-        
+          int opcion;
+
+        do {
+            try {
+                System.out.println("\n1. Agregar paciente");
+                System.out.println("2. Listar pacientes");
+                System.out.println("3. Salir");
+                System.out.print("Opcion: ");
+                opcion = Integer.parseInt(sc.nextLine());
+
+                switch(opcion) {
+                    case 1:
+                        System.out.print("Primer nombre: ");
+                        String nombre = sc.nextLine();
+
+                        System.out.print("Tipo documento: ");
+                        String tipoDoc = sc.nextLine();
+
+                        System.out.print("Numero documento: ");
+                        int numeroDoc = Integer.parseInt(sc.nextLine());
+
+                        System.out.print("Fecha nacimiento: ");
+                        String fecha = sc.nextLine();
+
+                        System.out.print("Email: ");
+                        String email = sc.nextLine();
+
+                        System.out.print("Telefono: ");
+                        String telefono = sc.nextLine();
+
+                        System.out.print("Alergias: ");
+                        String alergias = sc.nextLine();
+
+                        System.out.print("Tipo sangre: ");
+                        String tipoSangre = sc.nextLine();
+
+                        Paciente p = new Paciente(
+                                nombre, tipoDoc, numeroDoc,
+                                fecha, email, telefono,
+                                alergias, tipoSangre
+                        );
+
         
         
     }
