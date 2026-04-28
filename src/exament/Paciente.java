f2683027095913cb99cd742d0329ac87b43ea52c
TreizyHoyos <treizyyadhirahoyosjulca@gmail.com>
2026-04-27 20:26:46 -0500

    Creacion de variables y constructor

--- src/exament/Paciente.java
+++ src/exament/Paciente.java
@@ -10,5 +10,27 @@ package exament;
  */
 public class Paciente {
     
+    private String primer_nombre;
+    private String tipo_documento;
+    private int numero_documento;
+    private String fecha_nacimiento;
+    private String email;
+    private String telefono;
+    private String alergias;
+    private String tipo_sangre;
+
+    public Paciente(String primer_nombre, String tipo_documento, int numero_documento,
+                    String fecha_nacimiento, String email, String telefono,
+                    String alergias, String tipo_sangre) {
+
+        this.primer_nombre = primer_nombre;
+        this.tipo_documento = tipo_documento;
+        this.numero_documento = numero_documento;
+        this.fecha_nacimiento = fecha_nacimiento;
+        this.email = email;
+        this.telefono = telefono;
+        this.alergias = alergias;
+        this.tipo_sangre = tipo_sangre;
+    }
     
 }
